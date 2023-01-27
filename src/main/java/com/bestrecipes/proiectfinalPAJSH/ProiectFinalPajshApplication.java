fc8abc751becfd8fc6004adcd364975c9502cd00
Sorin <email>
2023-01-27 21:15:56 +0200

    initcommit

---
+++ src/main/java/com/bestrecipes/proiectfinalPAJSH/ProiectFinalPajshApplication.java
@@ -0,0 +1,14 @@
+package com.bestrecipes.proiectfinalPAJSH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectFinalPajshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProiectFinalPajshApplication.class, args);
+	}
+
+}
+    git config --global user.signingKey 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDduOoCANKo7zOwa7S+Q8NffL6Kjk5KKjy1aA3GlNUrkmMYI6aiJ8JVgcH+4cSkEs530/o4ycu7jitC886Z7jgXtpo/mavXoRwHttNk8DbSFE7KSsUUIG/thk4vq8Hhv04rVf/vW2OJ3ySF9x8lVlaIjEHXMg2VuO4gJM+PR/HnMaF7QDTfnZoRddoKMsESDQeRA3IKmLI6PL6hG9Za7fZRiTdh8EmIMk42SFKPuvUAchnyCdHDCg6FSwLHRwXkdMsHy4u+As+QmulTm+lsFM8fCK8VSnM5TSXeIta5VGo1h6GWJtIShpYALh+e0gMwdWBlZOteXq8FJVojC/ng7YSrmBb0oH0UI9pL6qL1p2Am2xNOgsHNE0mbTgtsqkJ+qL5NslwHvaBBmwowJYSOIUaqkqm42bsQM9+a6ErBUfH++e+IKShXs5+qgkD9f5E8jVWKo7wgdJ/Voj3kdAOSIivqbSGwP+1GF96vqBJrqo72NyljVmGoVfMM+4aI8I0tLm8= Sorin.Sr'
